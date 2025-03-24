e9b83f98391536d49fddb0ddc4abd6a3148e4422
Saravana Kumar M <kumar9885035454@gmail.com>
2025-03-24 17:54:12 +0530

    2nd commit

--- src/test/java/tech/badprogrammer/CalculatorTest.java
+++ src/test/java/tech/badprogrammer/CalculatorTest.java
@@ -16,4 +16,16 @@ class CalculatorTest {
         Calculator calculator = new Calculator();
         Assertions.assertEquals(4, calculator.add(2, 2));
     }
+
+    @Test
+    void test_subtract_001() {
+        Calculator calculator = new Calculator();
+        Assertions.assertEquals(0, calculator.subtract(1, 1));
+    }
+
+    @Test
+    void test_subtract_002() {
+        Calculator calculator = new Calculator();
+        Assertions.assertEquals(1, calculator.subtract(2, 1));
+    }
 }
